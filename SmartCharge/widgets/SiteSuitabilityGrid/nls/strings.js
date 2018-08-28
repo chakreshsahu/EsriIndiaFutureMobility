1a8eccd7cf7d4358d2d778cb6b97e46b4e604eb1
manjari188 <manjari.dhoundiyal@esriindia.com>
2018-08-28 09:49:41 +0530

    App renamed and widget removed
    Smart charge has been changed to eSmart charge everywhere.
    Site suitability grid widget has been removed.

--- SmartCharge/widgets/SiteSuitabilityGrid/nls/strings.js
+++
@@ -1,8 +0,0 @@
-define({
-  root: {
-    widgetTitle: 'Site Suitability Grid',
-    description: 'A Thematic map grid of site suitablity for electric vehicle charge station'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-});
